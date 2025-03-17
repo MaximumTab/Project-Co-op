a5aa0a93f82f12e0a98e4ff22242faff1b1ee201
Jay <Byrnejay2001@gmail.com>
2025-03-18 00:26:35 +1100

    CameraUpdate

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -2,35 +2,50 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour
 {
-    
     [SerializeField] private Transform target;
     [SerializeField] private Vector3 offset;
     [SerializeField] private float height;
 
     private Quaternion targetRotation;
+
     private float yRotation;
     private float xRotation;
     private float xRotationClamped;
-    [SerializeField]private float xRotationMin;
-    [SerializeField]private float xRotationMax;
 
+    [SerializeField] private float xRotationMin;
+    [SerializeField] private float xRotationMax;
+
+    [SerializeField] private float xSensitivity;
+    [SerializeField] private float ySensitivity;
+
+    [SerializeField] private bool invertY;
+    private int yInvertedValue;
+
+    private Vector3 desiredPos;
 
     private void Start()
     {
         Cursor.visible = false;
         Cursor.lockState = CursorLockMode.Locked;
+
+        yInvertedValue = invertY ? 1 : -1; 
     }
 
-    private void Update() 
+    private void Update()
     {
-        yRotation += Input.GetAxis("Mouse X");
-        xRotation += Input.GetAxis("Mouse Y");
+        yRotation += Input.GetAxis("Mouse X") * ySensitivity;
+        xRotation -= Input.GetAxis("Mouse Y") * xSensitivity * yInvertedValue; 
     }
-    void LateUpdate()
+
+    private void LateUpdate()
     {
         xRotationClamped = Mathf.Clamp(xRotation, xRotationMin, xRotationMax);
-        targetRotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
-        transform.position = target.position - targetRotation * offset + Vector3.up;
-        transform.rotation = targetRotation;
+        targetRotation = Quaternion.Euler(xRotationClamped, yRotation, 0.0f);
+
+        desiredPos = target.position - targetRotation * offset + Vector3.up * height;
+
+        transform.SetPositionAndRotation(desiredPos, targetRotation);
     }
+
+    public Quaternion YRotation => Quaternion.Euler(0.0f, yRotation, 0.0f);
 }
