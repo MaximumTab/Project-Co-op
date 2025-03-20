3f73572d4360b8aa3e3a64acd6a03c46f9a27903
BDragonSoldier <ricky.oberholzer@gmail.com>
2025-03-20 13:00:33 +1100

    As a player I want to have stats that reflect my defensive and offensive abilities

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,62 @@
+using System;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class PlayerStats : MonoBehaviour
+{
+    [SerializeField,Range(1,100)] private int PCurLvl = 1;
+    private int PMaxLvl = 100;
+    [SerializeField] private float PBaseMaxExp = 100;
+    [SerializeField] private float PMultExp = 5;
+    private float PCurExp = 0;
+    [SerializeField] private float PMaxExp;
+    [SerializeField] private float PMaxHp;
+    private float PCurHP;
+    [SerializeField] private float PCurAtk;
+    
+    
+    private GameObject Player;
+    private GameObject Weapon;//Weapon to be added
+    private class PLvl
+    {
+        
+        public float PBaseHp {  get; private set; } = 100;
+        public float PAddHp {  get; private set; } = 20;
+        public float PBaseAtk { get; private set; } = 20;
+        public float PAddAtk {  get; private set; } = 2;
+
+        public PLvl()
+        {
+            
+        }
+        public PLvl(float PBH,float PAH,float PAA,float PBA)
+        {
+            PBaseHp = PBH;
+            PAddHp = PAH;
+            PBaseAtk = PBA;
+            PAddAtk = PAA;
+        }
+    }
+
+    private PLvl myPLvl;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        Player = gameObject.GetComponentInChildren<CharacterMovement>().gameObject;
+        //Weapon = gameObject.GetComponentInChildren<Weapon>().gameObject;
+        myPLvl = new PLvl();
+
+    }
+
+    private void Update()
+    {
+        LvlStats();
+    }
+
+    void LvlStats()//To be fully implemented further
+    {
+        PMaxExp = PBaseMaxExp * PMultExp * (PCurLvl-1);
+        PMaxHp = myPLvl.PBaseHp + myPLvl.PAddHp * (PCurLvl-1);
+        PCurAtk = myPLvl.PBaseAtk + myPLvl.PAddAtk * (PCurLvl-1);
+    }
+}
