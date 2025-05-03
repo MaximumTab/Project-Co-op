fa2035b6232fdc89ad293ac9b61bc40239fa3510
Jay <Byrnejay2001@gmail.com>
2025-05-03 23:09:21 +1000

    SoundUpdate and new update

---
+++ Assets/Scripts/Audio/AudioTrigger.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class AudioTrigger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public AudioSource audioSource;
+    public GameObject linkedPrefab;
+
+     private void Update()
+    {
+        
+        if (linkedPrefab == null && audioSource.isPlaying)
+        {
+            audioSource.Stop();
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player" && !audioSource.isPlaying)
+        audioSource.Play();
+    }
+}
